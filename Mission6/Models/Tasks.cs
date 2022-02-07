97ee0f745e72e45515a83e2a35c54404084c8e3b
aberg21 <aberg1915@gmail.com>
2022-02-07 15:35:57 -0700

    Models

---
+++ Mission6/Models/Tasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models
+{
+    public class Tasks
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+
+        [Required]
+        public string Task { get; set; }
+
+        
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+
+        //Build Foreign key Relationship
+        
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+
+        public bool Completed { get; set; }
+
+
+        //Build Foreign key Relationship
+        
+    }
+}
+
+
+ 
--- Mission6/Startup.cs
+++ Mission6/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission6.Models;
 
 namespace Mission6
 {
@@ -24,6 +26,11 @@ namespace Mission6
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<TaskDataContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:TheConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
