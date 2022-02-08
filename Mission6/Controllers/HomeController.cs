f1890a29d53090cbb9d37c9513a93c0825aab80d
lexi-jane <alexis.transfiguracion@gmail.com>
2022-02-07 17:25:35 -0700

    controller

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -11,11 +11,18 @@ namespace Mission6.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        // public HomeController(ILogger<HomeController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        private TaskDataContext TaskContext {get; set;}
+
+        public HomeController(TaskDataContext task_data)
         {
-            _logger = logger;
+            TaskContext = task_data
         }
 
         public IActionResult Index()
@@ -23,15 +30,74 @@ namespace Mission6.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Tasks()
         {
+            ViewBag.Tasks = TaskContext.Category.ToList()
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Tasks(Tasks task)
+        {
+            ViewBag.Tasks = TaskContext.Category.ToList()
+            TaskContext.Add(task);
+            TaskContext.SaveChanges();
+            return View("TaskList");
+
         }
 
         public IActionResult TaskList()
         {
-            return View();
+            ViewBag.Tasks = TaskContext.Category.ToList()
+
+            var tasks = TaskContext.Tasks
+                .Include(x => x.Category)
+                .Where(x => x.Completed == False)
+                .ToList();
+
+            return View(tasks);
+                
         }
+
+        // EDIT 
+        [HttpGet]
+        public IActionResult Edit(int taskid) // change to whatever edit file david named
+        {
+            ViewBag.Tasks = TaskContext.Category.ToList()
+            var task = TaskContext.Tasks.Single( x => x.TaskID == taskid);
+
+            return view("TaskList", task);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Tasks task) // change to whatever edit file david named
+        {
+            TaskContext.Update(task);
+            TaskContext.SaveChanges();
+            return RedirectToAction("TaskList");
+
+        }
+        
+        // DELETE
+
+        // [HttpGet]
+        // public IActionResult Delete(int taskid)
+        // {
+        //     var task = TaskContext.Tasks.Single(x => x.TaskID == taskid);
+        //     return View(task);
+        // }
+
+        [HttpPost]
+        public IActionResult Delete(Tasks task)
+        {
+            TaskContext.Tasks.Remove(task);
+            TaskContext.SaveChanges();
+            return RedirectToAction("TaskList");
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
